b1c21e688c43b485663da515e6d7ddaca00fe714
Sikandar Gaba <gabas@PHARMEVO.COM>
2025-06-11 00:46:16 +0500

    Fix: Alias '@' to 'src' for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 👈 this is the fix
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
